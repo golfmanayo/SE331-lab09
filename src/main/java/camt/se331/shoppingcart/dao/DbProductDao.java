6d41c6c6e06663b4e0adfb22dbceab1e3711750f
Nattawood <golfmanayo@gmail.com>
2015-04-08 11:39:32 +0700

    update 3

--- src/main/java/camt/se331/shoppingcart/dao/DbProductDao.java
+++ src/main/java/camt/se331/shoppingcart/dao/DbProductDao.java
@@ -3,7 +3,6 @@ package camt.se331.shoppingcart.dao;
 import camt.se331.shoppingcart.entity.Product;
 import camt.se331.shoppingcart.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
